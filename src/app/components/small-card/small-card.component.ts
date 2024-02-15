6deacfb03bb5913cc34a9c05c007807cb07c4f52
edsonmizuma <edson.mizuma@gmail.com>
2024-02-15 10:34:16 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -12,6 +12,8 @@ export class SmallCardComponent implements OnInit {
   cardDate: string = '';
   @Input()
   cardTitle: string = '';
+  @Input()
+  id: string = '0';
 
   constructor() {}
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo Homem de ferro em 4D',
+    description: 'Marvel anuncia um novo filme do homem de ferro, confira',
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqxN0x5wjSNvBZrdqIdlf52FjaDRcWL1fEeg&usqp=CAU',
+  },
+  {
+    id: '2',
+    title: 'Nova série anunciada no Disney',
+    description: 'blá blá blá',
+    photoCover: 'https://media.wizards.com/2023/images/daily/y01jw4VZjkDo.jpg',
+  },
+];
